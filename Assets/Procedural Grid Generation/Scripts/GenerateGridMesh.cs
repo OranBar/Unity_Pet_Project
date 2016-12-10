00bc181838a56e0384502b85fb124381605c46e0
Oran <baroran@hotmail.com>
2016-12-10 15:04:22 +0100

    Added prodecural grid mesh generation

---
+++ Assets/Procedural Grid Generation/Scripts/GenerateGridMesh.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GenerateGridMesh : MonoBehaviour {
+
+	public int xSize, ySize;
+	public Color GizmoColor = Color.black;
+
+	private Vector3[] vertices;
+	private Mesh mesh;
+
+	void Awake(){
+		mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		mesh.name = "Procedural Grid";
+		StartCoroutine(Generate());
+	}
+
+	public IEnumerator Generate(){
+		yield return StartCoroutine(GenerateVerticesAndUvs());
+		yield return StartCoroutine(GenerateTriangles());
+		mesh.RecalculateNormals();
+	}
+
+	private IEnumerator GenerateVerticesAndUvs(){
+		vertices = new Vector3[(xSize+1) * (ySize+1)];
+		Vector2[] uvs = new Vector2[vertices.Length];
+		Vector4[] tangents = new Vector4[vertices.Length];
+		Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+		int i = 0;
+		for (int y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++) {
+				vertices[i] = new Vector3(x,y);
+				uvs[i] = new Vector2((float)x/xSize, (float)y/ySize);
+				tangents[i] = tangent;
+				i++;
+				yield return new WaitForSeconds(0.02f);
+			}
+		}
+
+		mesh.vertices = vertices;
+		mesh.uv = uvs;
+		mesh.tangents = tangents;
+		yield return null;
+	}
+
+	public IEnumerator GenerateTriangles(){
+
+		int[] triangles = new int[ySize * xSize*6];
+
+		for(int y=0, ti=0, vi=0; y < ySize; y++, vi++){
+			for(int x=0; x < xSize; x++, ti+=6, vi++){
+				triangles[ti] = vi+0;
+				triangles[ti+1] = triangles[ti+4] = xSize +vi+1;
+				triangles[ti+2] = triangles[ti+3] = vi+1;
+				triangles[ti+5] = xSize +vi+2;
+				mesh.triangles = triangles;
+				yield return new WaitForSeconds(0.05f);
+			}
+		}
+		yield return null;
+	}
+
+
+	private void OnDrawGizmos(){
+		if(vertices == null){
+			return;
+		}
+		Gizmos.color = GizmoColor;
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.DrawSphere(this.transform.position + vertices[i], 0.1f);
+		}
+	}
+}
---
+++ Assets/Procedural Grid Generation/Scripts/TerrainPerlinNoise.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class TerrainPerlinNoise : MonoBehaviour {
+
+	public float width = 1000f, height = 1000f;
+	public int scale = 1000;
+
+	public float perlinNoiseTranslation = 0;
+
+	// Use this for initialization
+	void Start () {
+		CreateMesh();
+	}
+
+	protected void CreateMesh(){
+		this.gameObject.AddComponent<MeshFilter>();
+		this.gameObject.AddComponent<MeshRenderer>();
+
+		Mesh mesh = new Mesh();
+
+		int rows = (int)(height/scale);
+		int cols = (int)(width/scale);
+
+		print("rows "+ rows);
+		print("cols "+ cols);
+
+		Vector3[] vertices = new Vector3[rows*cols*4];
+		Vector2[] newUV = new Vector2[rows*cols*4];
+		Vector3[] normals = new Vector3[rows*cols*4];
+
+		int[] tri = new int[rows*cols*6];
+
+		int currVertex = 0;
+
+		for (int y = 0; y < cols-1; y++) {
+			for (int x = 0; x < rows-1; x++) {
+
+				vertices[currVertex] = new Vector3(x*scale, y*scale, 0);
+				vertices[currVertex+1] = new Vector3((x+1)*scale, y*scale, 0);
+				vertices[currVertex+2] = new Vector3(x*scale, (y+1)*scale, 0);
+				vertices[currVertex+3] = new Vector3((x+1)*scale, (y+1)*scale, 0);
+
+				/*
+				print("x*scale "+ x*scale);
+				print("(x+1)*scale "+ ((x+1)*scale));
+				print("y*scale "+ y*scale);
+				print("(y+1)*scale "+ ((y+1)*scale));
+
+				print("Vectors");
+				vertices.ToList()
+					.ForEach(v=>Debug.Log(v));
+				*/
+
+
+
+
+				//  Lower left triangle.
+				tri[currVertex] = currVertex;
+				tri[currVertex+1] = currVertex+2;
+				tri[currVertex+2] = currVertex+1;
+
+				//  Upper right triangle.   
+				tri[currVertex+3] = currVertex+2;
+				tri[currVertex+4] = currVertex+3;
+				tri[currVertex+5] = currVertex+1;
+
+				currVertex += 4;
+			}
+			print("row "+y+" done");
+		}
+
+
+
+		GetComponent<MeshFilter>().mesh = mesh;
+		mesh.vertices = vertices;
+		mesh.uv = newUV;
+		mesh.normals = normals;
+		mesh.triangles = tri;
+	}
+
+
+
+
+}
