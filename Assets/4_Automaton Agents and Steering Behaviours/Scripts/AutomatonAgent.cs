b30f8d2a78b53e39b178c394a24127da7cb2422f
Oran <baroran@hotmail.com>
2016-12-11 20:13:18 +0100

    New Project: Automaton Agents and Steering Behaviours
    Added seek scene, and another scene of multiple agents seeking a moving object

---
+++ Assets/4_Automaton Agents and Steering Behaviours/Scripts/AutomatonAgent.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AutomatonAgent : MonoBehaviour {
+
+	public Transform target;
+	public float maxSpeed = 100f;
+
+	private Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody>();
+		myRigidBody.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Seek();
+
+		this.transform.LookAt(target);
+	}
+
+	public void Seek(){
+		Vector3 target = Seek_ChooseTarget();
+		Vector3 desiredVelocity = Seek_GetSteering(target);
+		Seek_Locomotion(desiredVelocity);
+	}
+
+	private Vector3 Seek_ChooseTarget(){
+		return target.position;
+		
+	}
+
+	/** <summary Returns desired velocity />
+	* 
+	*/
+	private Vector3 Seek_GetSteering(Vector3 target){
+		Vector3 vectorToTarget = target - this.transform.position;
+		Vector3 desiredVelocity = vectorToTarget.normalized * maxSpeed;
+		Vector3 steer = desiredVelocity - myRigidBody.velocity;
+
+		return steer;
+	}
+
+	private void Seek_Locomotion(Vector3 desiredVelocity){
+		myRigidBody.AddForce(desiredVelocity);
+	}
+
+}
---
+++ Assets/4_Automaton Agents and Steering Behaviours/Scripts/RandomMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class RandomMovement : MonoBehaviour {
+
+	public int changeDirectionFrequencyInFrames = 60;
+	public float speed = 1;
+
+	private Vector3 currTarget;
+
+	private int frame = 0;
+
+	public void Update(){
+		unchecked{
+			frame++;
+		}
+		if(frame%changeDirectionFrequencyInFrames == 0){
+			
+			currTarget = new Vector3(Random.Range(2, 50), 
+				Random.Range(2, 50),
+				Random.Range(2, 50));
+		}
+
+		Vector3 dirToTarget = (currTarget - this.transform.position).normalized;
+		this.transform.position = this.transform.position + dirToTarget * speed;
+
+
+	}
+
+
+}
