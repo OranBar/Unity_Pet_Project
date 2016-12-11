facfe2cc958c112bb855e5851ea38ef1bc102969
Oran <baroran@hotmail.com>
2016-12-11 20:12:10 +0100

    Added WireFrame to Terrain procedural Generation. It now looks cool as fuck

---
+++ Assets/2_Perlin Noise - Terrain Procedural Generation/Materials/WireframeRendering.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WireframeRendering : MonoBehaviour {
+
+	public bool enable = true;
+
+	void OnPreRender() {
+		GL.wireframe = enable;
+	}
+
+	void OnPostRender() {
+		GL.wireframe = false;
+	}
+}
