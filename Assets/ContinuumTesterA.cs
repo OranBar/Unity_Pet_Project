55bc7e72c70afe5cb86a5d8a7a168b67d1854d24
Oran <baroran@hotmail.com>
2017-07-27 01:34:58 +0200

    .

---
+++ Assets/ContinuumTesterA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuumTesterA : MonoBehaviour {
+
+	public void CanYouCallMe()
+	{
+		Debug.Log("Good job");
+	}
+
+	private void NinjaMethod()
+	{
+		Debug.Log("This is a shame to all of my ninja friends");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
