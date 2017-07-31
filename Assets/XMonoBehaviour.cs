74e2392f84ff6fb76f5ab79b7c0405d688e9795b
Oran <baroran@hotmail.com>
2017-07-31 15:04:25 +0200

    .

---
+++ Assets/XMonoBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+
+public abstract class XMonoBehaviour : MonoBehaviour
+{
+
+	//bool initialized_awake = false;
+
+	public void Awake()
+	{
+		
+
+		//initialized_awake = true;
+		XAvake();
+	}
+
+	public void Start()
+	{
+
+		XStart();
+	}
+
+	protected virtual void XAvake() { }
+
+	protected virtual void XStart() { }
+
+
+
+}
