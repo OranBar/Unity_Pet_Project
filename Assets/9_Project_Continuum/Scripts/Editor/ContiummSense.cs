db4554cf3678c0e0d7f140f8974b875c40ce6565
Oran <baroran@hotmail.com>
2017-08-09 01:02:37 +0200

    Added Debug wrapper to stop debugging by changing the wrapper's bool

--- Assets/9_Project_Continuum/Scripts/Editor/ContiummSense.cs
+++ Assets/9_Project_Continuum/Scripts/Editor/ContiummSense.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Reflection;
+using Debug = TonRan.Continuum.Debug;
 
 namespace TonRan.Continuum
 {
---
+++ Assets/9_Project_Continuum/Scripts/Editor/Debug.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TonRan.Continuum
+{
+	public class Debug {
+
+		public static bool enabled = false;
+
+		private UnityEngine.Debug debug;
+
+		public static void Log(object o)
+		{
+			if (enabled)
+			{
+				Debug.Log(o); 
+			}
+		}
+
+		public static void LogFormat(object o)
+		{
+			if (enabled)
+			{
+				Debug.LogFormat(o); 
+			}
+		}
+
+		public static void LogError(object o)
+		{
+			if (enabled)
+			{
+				Debug.LogError(o); 
+			}
+		}
+
+		public static void LogErrorFormat(object o)
+		{
+			if (enabled)
+			{
+				Debug.LogErrorFormat(o); 
+			}
+		}
+
+		public static void LogWarning(object o)
+		{
+			if (enabled)
+			{
+				Debug.LogWarning(o); 
+			}
+		}
+
+		public static void LogWarningFormat(object o)
+		{
+			if (enabled)
+			{
+				Debug.LogErrorFormat(o); 
+			}
+		}
+
+		public static void Assert(object o)
+		{
+			if (enabled)
+			{
+				Debug.Assert(o);
+			}
+		}
+
+		public static void Assert(object o, string msg)
+		{
+			if (enabled)
+			{
+				Debug.Assert(o, msg);
+			}
+		}
+
+
+	}
+
+}
