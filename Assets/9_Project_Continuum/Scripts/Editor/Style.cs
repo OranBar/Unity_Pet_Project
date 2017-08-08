fa30f3988b25d10c409806244f957103623e4736
Oran <baroran@hotmail.com>
2017-08-08 23:52:08 +0200

    Merge branch 'release/a1.4' into production_continuum

---
+++ Assets/9_Project_Continuum/Scripts/Editor/Style.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "CurrStyle", menuName = "Continuum/CreateStyle", order = 1)]
+public class Style : ScriptableObject {
+
+	public GUIStyle style;
+
+	
+}
+
+public class StyleCreator
+{
+	[MenuItem("Assets/Create/Style")]
+	public static void CreateStyle()
+	{
+		Style newStyle = ScriptableObject.CreateInstance<Style>();
+
+		AssetDatabase.CreateAsset(newStyle, "Assets/9_Project_Continuum/Styles/NewStyle.asset");
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = newStyle;
+	}
+}
+
+#endif
