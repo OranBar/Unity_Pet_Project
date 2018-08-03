41888a823a4e3ea0a06079f22db2cb32a486cec5
Oran <baroran@hotmail.com>
2018-08-03 15:36:22 +0200

    auto update

---
+++ Assets/OBLib/Programming/Oran/Scripts/AutoAttribute/TestAssets/Tester_AutoMulti.cs
@@ -0,0 +1,31 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Tester_AutoMulti : MonoBehaviour {
+
+	[AutoChildren] private Light myLight;
+	[AutoChildren] private Collider[] myChildrenCollidersArr;
+	[AutoChildren] public Collider[] MyChildrenCollidersArr { get; set; }
+	[AutoParent] private Collider[] myParentCollidersArr;
+
+	[AutoChildren] private List<Collider> myChildrenCollidersList;
+
+	// Use this for initialization
+	[Button]
+	void RunTest () {
+		AutoAttributeManager.AutoReference(this);
+
+		Assert.IsNotNull(myChildrenCollidersArr);
+		Assert.IsNotNull(MyChildrenCollidersArr);
+		Assert.AreEqual(1, myParentCollidersArr.Length); //Parent of this object has one collider
+		Assert.IsNotNull(myChildrenCollidersList);
+		Assert.IsNotNull(myLight);   //Child has light component
+		Assert.AreEqual(2, myChildrenCollidersArr.Length);   //This gameobject has 2 children colliders
+		Assert.AreEqual(2, MyChildrenCollidersArr.Length);   //This gameobject has 2 children colliders
+		Assert.AreEqual(2, myChildrenCollidersList.Count);	
+	}
+
+}
---
+++ Assets/OBLib/Programming/Oran/Scripts/AutoAttribute/TestAssets/Tester_SingleAuto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester_SingleAuto : MonoBehaviour {
+
+	[Auto] public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
